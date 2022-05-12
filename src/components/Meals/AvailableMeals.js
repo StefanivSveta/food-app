9a6f28012a33d5ec5670f72fa7579f8dca3d9252
StefanivSveta <svitlana04081998@gmail.comm>
2022-05-12 15:01:45 +0300

    Handling Errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,59 @@
 import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card.js'
+import Card from "../UI/Card.js";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-      const fetchMeals = async () => {
-      const response = await fetch('https://food-app-b7546-default-rtdb.firebaseio.com/meals.json')
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-app-b7546-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
 
-      const loadedMeals = [] 
+      const loadedMeals = [];
 
-      for (const key in responseData){
+      for (const key in responseData) {
         loadedMeals.push({
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
           price: responseData[key].price,
-        })
+        });
       }
-      setMeals(loadedMeals)
-      setIsLoading(false)
-    }
-    fetchMeals();
-  }, [])
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
 
-  if(isLoading){
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
       </section>
-    )
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>Failed to fetch</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map((meal) => (
